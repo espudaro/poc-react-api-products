63c47bfe12073c1d2f1524d0404a73554d797fec
Bruno Espudaro <bruno.espudaro@gaveteiro.com.br>
2019-06-13 15:34:49 -0300

    lista produtos com react

---
+++ app/javascript/components/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://54.207.97.97:9090/products')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json.products,
+      })
+    });
+  }
+
+  render() {
+
+    var { items, isLoaded } = this.state;
+    if (!isLoaded) {
+      return <div> Loading...</div>;
+    } else {
+      return (
+        <ul className='list-products'>
+          {items.map(item => (
+            <li key={item.sku} className='product__item'>
+              <img src={item.media} className='product__item--img' />
+              <h3 className='product__item--name'>{item.name}</h3>
+              <div className='product__item--sku'>{item.sku}</div>
+              <p className='product__item--description'>{item.description}</p>
+              <div className='product__item--amount'>{item.amount}</div>
+            </li>
+          ))}
+        </ul>
+      );
+
+    }
+
+  }
+
+
+
+
+  // componentDidMount() {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       this.setState({
+  //         isLoaded: true,
+  //         items: json,
+  //       })
+  //     });
+  // }
+
+  // render() {
+
+  //   var { items, isLoaded } = this.state;
+  //   if (!isLoaded) {
+  //     return <div> Loading...</div>;
+  //   } else {
+  //     return (
+  //       <div className='App'>
+  //         <ul>
+  //           {items.map(item => (
+  //             <li key={item.name}>
+  //               {item.name}
+  //             </li>
+  //           ))}
+  //         </ul>
+  //       </div>
+  //     )
+
+  //   }
+
+  // }
+}
+
+export default App;
